c1c92c3f43a389b4f2d067eee885c9dbcc012b7f
meslall <meslalla90@gmail.com>
2024-04-02 22:06:09 +0000

    header_done

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import './brand.css'
+import React from "react";
+// eslint-disable-next-line no-unused-vars
+import { google, dropbox, shopify, slack, atlassian } from "./brands";
+import "./brand.css";
 
 const Brand = () => {
   return (
-    <div>Brand</div>
-  )
-}
+    <div className="brand_container section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+    </div>
+  );
+};
 
-export default Brand
+export default Brand;
