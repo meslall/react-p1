fbb36814afd1ee30e05caa061ec5580aba24f208
meslall <meslalla90@gmail.com>
2024-04-15 23:41:36 +0100

    ADD_possibility

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -3,7 +3,13 @@ import './cta.css'
 
 function Cta() {
   return (
-    <div>Cta</div>
+    <div className='cta_container section__margin'>
+      <div className='cta_container-text'>
+        <p>Request Early Access to Get Started</p>
+        <h1>Register today & start exploring the endless possiblities.</h1>
+      </div>
+      <button className="button-">Get Started</button>
+    </div>
   )
 }
 
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './possibility.css'
+import possibility from '../../assets/possibility.png'
 
 function Possibility() {
   return (
-    <div>Possibility</div>
+    <div className='possibility_container section__margin'>
+      <div className='possibility_container-image'>
+        <img src= {possibility} alt="possibility" />
+      </div>
+      <div className='possibility_container-text'>
+        <p>Request Early Access to Get Started</p>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <p id='sub'>Request Early Access to Get Started</p>
+      </div>
+    </div>
   )
 }
 
